51123065cea27b6aef985c402205adcbf9d0652e
leekool <leejedda@gmail.com>
2023-03-07 23:24:52 +1100

    ignore commented lines

--- back/parse_keys.py
+++ back/parse_keys.py
@@ -1,9 +1,11 @@
+keys = ()
+
 with open("testmap.c", "r") as f:
     flag = False
     for line in f:
         if "MATRIX_COLS" in line:
             flag = True
-        if flag:
+        if flag and line.strip().startswith('//') == False:
             print(line)
         if flag == True and "};" in line:
             break
