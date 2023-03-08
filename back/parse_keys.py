b2f1bc18aeb8188d689c53242b48449c6d79f35a
leekool <leejedda@gmail.com>
2023-03-08 19:32:14 +1100

    Keymap class

--- back/parse_keys.py
+++ back/parse_keys.py
@@ -1,5 +1,10 @@
 keys = []
 
+class Keymap:
+    def __init__(self, *layers):
+        for i, layer in enumerate(layers):
+            setattr(self, f"layer{i+1}", layer)
+
 with open("testmap.c", "r") as f:
     flag = False
     for line in f:
