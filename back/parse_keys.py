38c003b5d3c10ee50aaf65140d872f71f010c5db
leekool <leejedda@gmail.com>
2023-03-19 16:32:40 +1100

    key_dict

--- back/parse_keys.py
+++ back/parse_keys.py
@@ -1,5 +1,57 @@
 import re
 
+# key_dict = {
+#     'XXXXXXX': '',
+#     'KC_A': 'A',
+#     'KC_B': 'B',
+#     'KC_C': 'C',
+#     'KC_D': 'D',
+#     'KC_E': 'E',
+#     'KC_F': 'F',
+#     'KC_G': 'G',
+#     'KC_H': 'H',
+#     'KC_I': 'I',
+#     'KC_J': 'J',
+#     'KC_K': 'K',
+#     'KC_L': 'L',
+#     'KC_M': 'M',
+#     'KC_N': 'N',
+#     'KC_O': 'O',
+#     'KC_P': 'P',
+#     'KC_Q': 'Q',
+#     'KC_R': 'R',
+#     'KC_S': 'S',
+#     'KC_T': 'T',
+#     'KC_U': 'U',
+#     'KC_V': 'V',
+#     'KC_W': 'W',
+#     'KC_X': 'X',
+#     'KC_Y': 'Y',
+#     'KC_Z': 'Z',
+#     'KC_1': '1',
+#     'KC_2': '2',
+#     'KC_3': '3',
+#     'KC_4': '4',
+#     'KC_5': '5',
+#     'KC_6': '6',
+#     'KC_7': '7',
+#     'KC_8': '8',
+#     'KC_9': '9',
+#     'KC_0': '0',
+#     'KC_F1': 'F1',
+#     'KC_F2': 'F2',
+#     'KC_F3': 'F3',
+#     'KC_F4': 'F4',
+#     'KC_F5': 'F5',
+#     'KC_F6': 'F6',
+#     'KC_F7': 'F7',
+#     'KC_F8': 'F8',
+#     'KC_F9': 'F9',
+#     'KC_F10': 'F10',
+#     'KC_F11': 'F11',
+#     'KC_F12': 'F12',
+# }
+
 class Layer:
     def __init__(self, name):
         self.name = name
@@ -37,6 +89,13 @@ for line in lines:
                 continue
 
             key = key.strip()
+
+            # if key in key_dict:
+            #     key = key_dict[key]
+
+            if 'KC_' in key:
+                key = key.replace('KC_', '')
+
             current_layer.keys.append(key)
 
     if ')' in line:
