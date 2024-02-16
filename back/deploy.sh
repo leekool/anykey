fae85257d4130b3c23c6442e0d2e1f39a42a96c0
leekool <lee@imre.al>
2024-02-16 20:42:34 +1100

    kill gunicorn workers

--- back/deploy.sh
+++ back/deploy.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-# this is no way an ideal way to run the app, but will do for now
-if kill $(pgrep -f "python3 app.py"); then
-    echo "Existing process killed successfully."
+# if kill $(pgrep -f "python3 app.py"); then
+#     echo "Existing process killed successfully."
+# else
+#     echo "No existing process found."
+# fi
+#
+# nohup python3 app.py > /dev/null 2>&1 &
+
+gunicorn_pids=$(pgrep -f "gunicorn")
+
+if [ -n "$gunicorn_pids" ]; then
+    for pid in $gunicorn_pids; do
+        if kill "$pid"; then
+            echo "gunicorn process $pid killed"
+        else
+            echo "failed to kill gunicorn process $pid"
+        fi
+    done
 else
-    echo "No existing process found."
+    echo "no existing gunicorn processes found"
 fi
 
-# nohup python3 app.py > /dev/null 2>&1 &
 gunicorn -w 4 -b "$ANYKEY_URL":5000 --certfile "$CERT_PATH" --keyfile "$KEY_PATH" app:app --daemon
